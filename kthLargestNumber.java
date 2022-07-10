61abed371ba28be22f7f6a24de45d136a29d91f2
saiteja akula <78987220+tejasai272@users.noreply.github.com>
2022-07-10 16:33:31 -0700

    Create kthLargestNumber.java

---
+++ kthLargestNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+  
+  //TC; O(Nlogk) SC: O(n)
+    class mycomparator implements Comparator<Integer>{
+        public int compare(Integer a,Integer b){
+            return b-a;
+        }
+    }
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(new mycomparator());
+        
+        for(int i: nums){
+            pq.add(i);
+        }
+        for(int j=1;j<k;j++)
+            pq.poll();
+        return pq.peek();
+    }
+}
